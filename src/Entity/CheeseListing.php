43ebfc053aeaf8bc1c90f7fb33619745ee8e18d8
Mykyta <>
2022-07-20 21:55:22 +0300

    Customize Cheese Listing

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -4,12 +4,17 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\CheeseListingRepository;
+use Carbon\Carbon;
 use DateTimeInterface;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CheeseListingRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    collectionOperations: ['get', 'post'],
+    itemOperations: ['get' => [], 'put', 'patch'],
+    shortName: 'cheeses'
+)]
 class CheeseListing
 {
     #[ORM\Id]
@@ -23,6 +28,9 @@ class CheeseListing
     #[ORM\Column(type: Types::TEXT)]
     private ?string $description = null;
 
+    /**
+     * The price of this delicious cheese, in cents.
+     */
     #[ORM\Column]
     private ?int $price = null;
 
@@ -31,6 +39,11 @@ class CheeseListing
 
     #[ORM\Column]
     private ?bool $isPublished = null;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
 
     public function getId(): ?int
     {
@@ -54,9 +67,9 @@ class CheeseListing
         return $this->description;
     }
 
-    public function setDescription(string $description): self
+    public function setTextDescription(string $description): self
     {
-        $this->description = $description;
+        $this->description = nl2br($description);
 
         return $this;
     }
@@ -78,11 +91,9 @@ class CheeseListing
         return $this->createdAt;
     }
 
-    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    public function getCreatedAtAgo(): string
     {
-        $this->createdAt = $createdAt;
-
-        return $this;
+        return Carbon::instance($this->createdAt)->diffForHumans();
     }
 
     public function isIsPublished(): ?bool
