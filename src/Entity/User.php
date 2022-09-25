7951339607c65b8f554f6e864acfe31c40795968
Mykyta <bondarchuk.nik.nb@gmail.com>
2022-09-25 21:54:19 +0300

    Add admin groups context builder and an admin group appropriately

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -67,7 +67,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $email = null;
 
     #[ORM\Column]
-    #[Groups(['user:write'])]
+    #[Groups(['admin:write'])]
     private array $roles = [];
 
     /**
--- tests/Functional/UserResourceTest.php
+++ tests/Functional/UserResourceTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Functional;
 
+use App\Entity\User;
 use App\Test\CustomApiTestCase;
 use Hautelook\AliceBundle\PhpUnit\ReloadDatabaseTrait;
 
@@ -34,12 +35,45 @@ class UserResourceTest extends CustomApiTestCase
         $client->request('PUT', '/api/users/' . $user->getId(), [
             'json' => [
                 'username' => 'newcheeseplease',
+                'roles' => ['ROLE_ADMIN'],
             ],
         ]);
         $this->assertResponseIsSuccessful();
 
         $this->assertJsonContains([
             'username' => 'newcheeseplease'
+        ]);
+
+        $em = $this->getEntityManager();
+        $user = $em->getRepository(User::class)->find($user->getId());
+        $this->assertEquals(['ROLE_USER'], $user->getRoles());
+    }
+
+    public function testGetUser()
+    {
+        $client = self::createClient();
+        $user   = $this->createUserAndLogIn($client, 'cheeseplease@example.com', 'foo');
+        $phoneNumber = '380671234567';
+        $user->setPhoneNumber($phoneNumber);
+        $em = $this->getEntityManager();
+        $em->flush();
+
+        $client->request('GET', '/api/users/' . $user->getId());
+        $this->assertJsonContains([
+            'username' => 'cheeseplease',
+        ]);
+
+        $data = $client->getResponse()->toArray();
+        $this->assertArrayNotHasKey('phoneNumber', $data);
+
+        $user = $em->getRepository(User::class)->find($user->getId());
+        $user->setRoles(['ROLE_ADMIN']);
+        $em->flush();
+        $this->logIn($client, $user->getEmail(), 'foo');
+
+        $client->request('GET', '/api/users/' . $user->getId());
+        $this->assertJsonContains([
+              'phoneNumber' => $phoneNumber,
         ]);
     }
 }
